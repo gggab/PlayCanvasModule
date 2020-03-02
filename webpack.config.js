3414ed66a1f3b0e94ea003991e50ebc2dca9418a
liaowentao <1421750225@qq.com>
2020-03-02 22:55:33 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,35 @@
 const path = require('path');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader,'css-loader']
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     },
     plugins:[
         new MiniCssExtractPlugin({
-            filename:'styles.css',
+            filename:'[name].css',
+            chunkFilename:'[id].css'
         }),
     ]
 }
